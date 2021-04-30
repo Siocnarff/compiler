d32c03fa47a3b81c3e7423da85ad36e094714b8b
siocnarff <josuabotha@protonmail.ch>
2021-04-30 17:53:18 +0200

    fixed error where 0 and 1 preferred over integer

--- app/controllers/parser/mapreader.rb
+++ app/controllers/parser/mapreader.rb
@@ -47,7 +47,11 @@ private
   def findDestinationColumn(food)
     index = 0
     @matrix[0].each do | transition |
-      if transition.eql? food[0] or transition.eql? food[1]
+      if @s == 38 and food[1].eql "0"
+        return 39
+      elsif @s == 47 and food[1].eql "1"
+        return 40
+      elsif transition.eql? food[0] or transition.eql? food[1]
         return index
       end
       index = index + 1
