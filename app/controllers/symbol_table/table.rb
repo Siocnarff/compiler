1f5ed89e03d56d59e6e8531cce37b631efa75625
siocnarff <josuabotha@protonmail.ch>
2021-05-04 13:29:07 +0200

    add for flag to table entries

--- app/controllers/symbol_table/table.rb
+++ app/controllers/symbol_table/table.rb
@@ -1,7 +1,5 @@
 class SymbolTable
   def initialize
-    @var_name_id_source = 0
-    @scope_id_source = 0
     @table_entries = Array.new
     @current_scope_depth = 0
     @scope = Array.new(
@@ -10,10 +8,13 @@ class SymbolTable
         start_pos = 0
       )
     )
+    # varibles used to genereate global unique ids
+    @var_name_id_source = 0
+    @scope_id_source = 0
   end
 
   def open_new_scope
-    @current_scope.push(
+    @scope.push(
       ScopeInfo.new(
         id = @scope_id_source += 1,
         start_pos = @table_entries.length
@@ -25,13 +26,14 @@ class SymbolTable
     @table_entries = @table_entries[0, @scope.pop.start_pos]
   end
 
-  def fetchOrFabricateInternalName(name, is_procedure)
+  def getOrGenerateInternalName(name, is_var, is_for_counter)
+    scope_string = self.generateScopeString
     @table_entries.each do |entry|
-      if entry.name.eql? name
+      if entry.name.eql? name and not entry.scope.eql? scope_string
         if entry.is_var?
-          raise "error: #{name} already defines a variable" if is_procedure
+          raise "error: #{name} already defines a variable" unless is_var
         else
-          raise "error: #{name} already defines a procedure" unless is_procedure
+          raise "error: #{name} already defines a procedure" if is_var
         end
         return entry.internal_name
       end
@@ -39,15 +41,28 @@ class SymbolTable
     @table_entries.push(
       TableEntry.new(
         name = name,
-        internal_name = generateInternalName(name, is_procedure)
+        internal_name = generateInternalName(name, is_var, is_for_counter),
+        is_for_counter = is_for_counter,
+        is_var = is_var,
+        scope_string = scope_string
       )
     )
     @table_entries.last
   end
 
+
 private
-  def generateInternalName(name, is_procedure)
-    if is_procedure
+
+  def generateScopeString
+    scope_string = ""
+    @scope.each do |s|
+      scope_string += "#{s.id}."
+    end
+    scope_string.chop
+  end
+
+  def generateInternalName(name, is_var)
+    unless is_var
       return name
     end
     "v#{@var_name_id_source += 1}"
@@ -70,11 +85,13 @@ class ScopeInfo
 end
 
 class TableEntry
-  def initialize(name, internal_name, is_for_counter = false, var = true)
+  def initialize(name, internal_name, is_for_counter, is_var, scope_string)
+    @scope_string
     @user_defined_name = name
     @internal_name = internal_name
     @is_for_counter = is_for_counter
     @var = var
+    @scope = scope_string
   end
 
   def internal_name
@@ -91,5 +108,9 @@ class TableEntry
 
   def is_var?
     @var
+  end
+
+  def scope
+    @scope
   end
 end
