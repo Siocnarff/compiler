24ce297294606ebe456f52099c098a60b3ea216e
siocnarff <josuabotha@protonmail.ch>
2021-05-22 08:08:26 +0200

    hotfix in naming

--- app/controllers/tree/tree.rb
+++ app/controllers/tree/tree.rb
@@ -112,9 +112,9 @@ class TokenGenerator
           ns = n.scope.split(".")
           nis= ni.scope.split(".")
           if n.terminals[0].eql? ni.terminals[0]
-            a = ns.length + 1 == ni.length and ns.last.eql?(ni[ni.length - 2])
-            b = ns.length - 1 == ni.length and ni.last.eql?(n[n.length - 2])
-            c = ns.length == ni.length and ns.last.eql?(ni.last)
+            a = ns.length + 1 == nis.length and ns.last.eql?(nis[ni.length - 2])
+            b = ns.length - 1 == nis.length and nis.last.eql?(n[n.length - 2])
+            c = ns.length == nis.length and ns.last.eql?(ni.last)
             if a or b or c
               if ni.is_a?(Call) and ni.terminal_types[0].eql?("UserDefinedInternalName")
                 lgr.info("HERE")
